821b04f4d4de4dcf4d481d86da81324a9a7718d1
Michael Blanco <89497253+MigueBlanc@users.noreply.github.com>
2023-01-14 18:16:17 -0500

    "FIXED I THINK"

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
-
+import Form from '../components/Form';
 const Contact = () => {
   return (
     <div>
        <Navbar/>
-       <HeroImg2 heading="Contact me here : " text="hank you for visiting my portfolio website. If you have any questions or would like to work with me, please feel free to reach out by filling out the contact form or by emailing me at [blancochavezm@g.cofc.edu]. 
+       <HeroImg2 heading="Contact me here : " text="Thank you for visiting my portfolio website. If you have any questions or would like to work with me, please feel free to reach out by filling out the contact form or by emailing me at [blancochavezm@g.cofc.edu]. 
        I look forward to hearing from you!"/>
+       <Form />
         <Footer /> 
     </div>
   )
